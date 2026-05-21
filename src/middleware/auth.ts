674e059593d1cc08270d4d1611ce9ecf625bea10
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-05-22 03:57:38 +0600

    Auth middleware validation

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,19 +1,47 @@
 import type { NextFunction, Request, Response } from "express";
-
-export const auth=(req:Request, res:Response, next:NextFunction)=>
-{
-  console.log(req.method,Date.now())
-  const token=req.headers.authorization;
-   if(!token){
+import type { JwtPayload } from "jsonwebtoken";
+import config from "../config";
+import jwt from "jsonwebtoken"
+import { Pool } from "pg";
+import { pool } from "../db";
+export const auth =()=>async (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.method, Date.now())
+  const token = req.headers.authorization;
+  if (!token) {
     res.status(401).json(
       {
-        success:false,
-        message :  "unauthorized access!!!"
-      }
-    )
+        success: false,
+        message: "unauthorized access!!!"
+      })
+  }
+  const decoded =jwt.verify(
+    token as string,
+    config.secret as string,
+  ) as JwtPayload;
+
+  const userData=await pool.query(`
     
-   }
-  next()
+    SELECT * FROM users WHERE email==$1
+    `,[decoded.email])
+  const user=userData.rows[0];
+  if(userData.rows[0].length===0)
+  {
+    res.status(404).json(
+      {
+        success: false,
+        message: "User not found!!!"
+      })
+  }
+  if(!user.is_active)
+  {
+    res.status(404).json(
+      {
+        success: false,
+        message: "Forbidden!!!"
+      })
+  }
  
+  next()
+
 
 }
--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -2,9 +2,9 @@ import type { NextFunction, Request, Response } from "express";
 import  fs  from "fs";
 
 const logger=(req:Request, res:Response, next:NextFunction) => {
-  console.log('Time:',req.method,req.url, Date.now());
+  // console.log('Time:',req.method,req.url, Date.now());
   const log= `\nMethod -> ${req.method}  Time -> ${Date.now()} URL -> ${req.url}\n`
-  console.log(log)
+
   fs.appendFile('logger.txt',log,(err)=>{
     console.log(err)
   })
