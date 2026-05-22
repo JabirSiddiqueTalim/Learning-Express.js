c63dccd492e714e475b6ebf7f32dc2bb825b825f
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-05-22 20:59:41 +0600

    Global Error Handling

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,8 @@ import { loginRouter } from "./modules/auth/auth.router";
 import fs from "fs";
 import logger from "./middleware/logger";
 const app: Application = express();
-import CookieParser from 'cookie-parser'
+import CookieParser from 'cookie-parser';
+import cors from "cors";
 app.get('/', (req: Request, res: Response) => {
   // res.send('Hello World I Am User!')
   res.status(200).json({
@@ -23,9 +24,21 @@ app.use(logger);
 app.use(express.json())
 app.use(express.text())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors({
+  origin: 'http://localhost:3000',
+}))
 
 app.use("/api/users",userRouter);
 app.use("/api/profile",profileRouter);
 app.use("/api/auth",loginRouter)
+
+app.use((err, req, res, next) => {
+  console.error(err.stack); // Log the error
+
+  res.status(500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+  });
+});
 
 export default app;
